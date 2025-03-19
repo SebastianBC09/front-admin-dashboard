59a9ed17e271289e095e3571e859d68ff2d64fd4
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-19 10:54:56 -0500

    fix(propiedades): folder renamed according to layout & routing

--- app/propiedades/page.tsx
+++ app/propiedades/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react';
 import { Typography, Box, List, ListItem, ListItemText, CircularProgress } from '@mui/material';
 import api from '../../services/api';
